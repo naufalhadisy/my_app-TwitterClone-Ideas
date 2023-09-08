462ba0329f23ba988ef9776ce50a927a1e415f5e
naufalhadisy <hadinaufal26@gmail.com>
2023-09-08 10:03:14 +0700

    + validation for 'share your ideas' form

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -9,6 +9,10 @@ class IdeasController extends Controller
 {
     public function store(){
 
+        request() -> validate([
+            'ideas' => 'required|min:3|max:255|'
+        ]);
+
         //comment this if want to stop generating new data to db
         $ideas = ideas::create(
             [
--- resources/views/shared/submit_ideas.blade.php
+++ resources/views/shared/submit_ideas.blade.php
@@ -4,6 +4,9 @@
     @csrf
     <div class="mb-3">
         <textarea name="ideas" class="form-control" id="ideas" rows="3" ></textarea>
+        @error('ideas')
+            <span class="d-block fs-6 mt-2 text-danger"> {{$message}} </span>
+        @enderror
     </div>
     <div class="">
         <button type="submit" class="btn btn-dark"> Share </button>
