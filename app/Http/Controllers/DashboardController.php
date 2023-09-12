448f2bbc8a945792f5f19d9ccca15931bb6e3838
naufalhadisy <hadinaufal26@gmail.com>
2023-09-12 09:26:06 +0700

    CRUD - Search Bar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,9 +9,18 @@ class DashboardController extends Controller
 {
     public function index(){
 
+        $ideas = ideas::orderBy('created_at', 'DESC');
+
+        //check if there is a search
+        //if there is, check the value based on database
+        if (request()->has('search')){
+
+            $ideas = $ideas->where('content', 'like', '%' . request()->get('search', '') . '%');
+        }
+
         return view('dashboard', [
             // 'ideas' => ideas::all()
-            'ideas' => ideas::orderBy('created_at', 'DESC')->paginate(5)
+            'ideas' =>$ideas->paginate(5)
         ]);
     }
 }
